a5bab5282faf8b354fff9c9135ddcbd5ff109fe5
josecgomes <jcgsg2000@gmail.com>
2022-12-06 21:01:39 +0000

    projeto final

---
+++ components/Home.js
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+export default HomeScreen
+
+
+/*
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+
+
+import Game from './components/Game';
+
+function Home({ navigation }) {
+  return (
+   <NavigationContainer>
+    <Text>ABC</Text>
+   </NavigationContainer>
+  );
+}
+
+export default Home;*/
